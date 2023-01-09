0137792be90ecf932c00b1d8e210ff1be90debd4
William Santos <willsantos.edf@gmail.com>
2023-01-09 17:09:33 -0300

    Criando django admin.

--- vendas/appvendas/admin.py
+++ vendas/appvendas/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from appvendas.models import *
 
-# Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'cpf']
+    search_fields = ['nome', 'email', 'cpf']
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 1
+
+
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'data']
+    inlines = [ItemInline]
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pedido', 'produto', 'quantidade', 'preco']
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Item, ItemAdmin)
+
--- vendas/appvendas/models.py
+++ vendas/appvendas/models.py
@@ -3,14 +3,21 @@ from django.db import models
 class Cliente(models.Model):
 
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
-    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(verbose_name='E-mail')
+    cpf = models.CharField(max_length=11, unique=True, verbose_name='CPF')
+
+    def __str__(self):
+        return self.nome
 
 
 class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, models.CASCADE)# CASCADE = Se apagar o cliente os dados serão apagados juntos.
     data = models.DateField()
-    observacao = models.TextField(blank=True) #blank = preenchimento não obrigatório
+    observacao = models.TextField(blank=True, verbose_name='Observação') #blank = preenchimento não obrigatório
+
+    def __str__(self):
+        return f'{self.cliente} #{self.id}'
+
 
 class Item(models.Model):
 
@@ -19,4 +26,6 @@ class Item(models.Model):
     quantidade = models.IntegerField(default=1)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f'[{self.quantidade}] {self.produto}'
 
